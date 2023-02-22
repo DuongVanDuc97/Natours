8fa4dcb99649b95ad53fda5b872b06dbb49c784f
Duc Duong <saxongngo@gmail.com>
2023-02-22 13:30:16 +0700

    update CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -86,6 +87,9 @@ app.use(
   })
 );
 app.use(compression());
+app.use(cors());
+// Access-Control-Allow-Origin *
+app.options('*', cors());
 
 // ROUTES
 app.use('/', viewRouter);
